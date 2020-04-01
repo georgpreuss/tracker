9e056970fdca882014b3418fcece5fa01bf45aef
Georg Preuss <preussg@gmail.com>
2020-04-01 10:57:36 +0100

    added track model and routes, express API complete

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,12 +1,17 @@
+require('./models/User')
+require('./models/Track')
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const authRoutes = require('./routes/authRoutes')
+const trackRoutes = require('./routes/trackRoutes')
+const requireAuth = require('./middlewares/requireAuth')
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use(authRoutes)
+app.use(trackRoutes)
 
 const mongoUri = 'mongodb+srv://georgpreuss:<password>@cluster0-dy84o.mongodb.net/test?retryWrites=true&w=majority'
 mongoose.connect(mongoUri, {
@@ -23,8 +28,8 @@ mongoose.connection.on('error', (err) => {
   console.error('Error connecting to mongo', err)
 })
 
-app.get('/', (req, res) => {
-  res.send('Hi there!')
+app.get('/', requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`)
 })
 
 app.listen(3000, () => {
